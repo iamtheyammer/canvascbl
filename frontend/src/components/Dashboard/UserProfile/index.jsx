51c162d04661319166117e2d91d552ff5d299ce2
iamtheyammer <iamtheyammer@gmail.com>
2019-11-04 17:44:11 -0800

    [Frontend] Mobilize and Functionalize UserProfile

--- frontend/src/components/Dashboard/UserProfile/index.jsx
+++ frontend/src/components/Dashboard/UserProfile/index.jsx
@@ -1,47 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
+import { isMobile } from 'react-device-detect';
 
-import { Spin, Descriptions } from 'antd';
+import { Spin, Descriptions, Typography } from 'antd';
+import { Card as MobileCard } from 'antd-mobile';
 
-class UserProfile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      getUserId: ''
-    };
-  }
+function UserProfile(props) {
+  const { user } = props;
 
-  render() {
-    if (!this.props.user || this.props.loading.includes(this.state.getUserId)) {
-      return (
-        <div align="center" style={{ marginTop: '20px' }}>
-          <Spin size="large" />
-        </div>
-      );
-    }
-
-    const user = this.props.user;
+  if (!user) {
     return (
-      <Descriptions title="My Profile" bordered>
-        <Descriptions.Item label="Name">{user.name}</Descriptions.Item>
-        <Descriptions.Item label="Email">
-          {user.primary_email}
-        </Descriptions.Item>
-        <Descriptions.Item label="ID">{user.id}</Descriptions.Item>
-        <Descriptions.Item label="Time Zone">
-          {user.time_zone}
-        </Descriptions.Item>
-      </Descriptions>
+      <div align="center" style={{ marginTop: '20px' }}>
+        <Spin size="large" />
+      </div>
     );
   }
+
+  if (isMobile) {
+    return (
+      <div>
+        <Typography.Title level={2}>My Profile</Typography.Title>
+        <MobileCard>
+          <MobileCard.Header title={user.name} thumb={user.avatar_url} />
+          <MobileCard.Body>
+            Email: {user.primary_email}
+            <br />
+            Time Zone: {user.time_zone}
+          </MobileCard.Body>
+          <MobileCard.Footer content={`ID: ${user.id}`} />
+        </MobileCard>
+      </div>
+    );
+  }
+
+  return (
+    <Descriptions title="My Profile" bordered>
+      <Descriptions.Item label="Name">{user.name}</Descriptions.Item>
+      <Descriptions.Item label="Email">{user.primary_email}</Descriptions.Item>
+      <Descriptions.Item label="ID">{user.id}</Descriptions.Item>
+      <Descriptions.Item label="Time Zone">{user.time_zone}</Descriptions.Item>
+    </Descriptions>
+  );
 }
 
 const ConnectedUserProfile = connect(state => ({
-  user: state.canvas.user,
-  token: state.canvas.token,
-  subdomain: state.canvas.subdomain,
-  error: state.error,
-  loading: state.loading
+  user: state.canvas.user
 }))(UserProfile);
 
 export default ConnectedUserProfile;
