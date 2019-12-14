eafdff2cb8a72be65f440ab7cba35820bbfb2844
iamtheyammer <iamtheyammer@gmail.com>
2019-12-14 11:26:15 -0800

    [F/B] Use string pattern matching for success skills

--- frontend/src/util/canvas/isSuccessSkillsOutcome.js
+++ frontend/src/util/canvas/isSuccessSkillsOutcome.js
@@ -1,1 +1,13 @@
-export default id => id === '2269' || id === '2270';
+export default title => {
+  const t = title.toLowerCase();
+
+  return (
+    t.startsWith('SS') ||
+    (t.includes('teacher') && t.includes('evaluation')) ||
+    (t.includes('teacher') && t.includes('assessment')) ||
+    (t.includes('self') && t.includes('evaluation')) ||
+    (t.includes('peer') && t.includes('evaluation')) ||
+    (t.includes('peer') && t.includes('assessment')) ||
+    (t.includes('self') && t.includes('evaluation'))
+  );
+};
