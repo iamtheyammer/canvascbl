77f14eb959536c3e15b40972dff4a6bc6e5fd1ce
iamtheyammer <iamtheyammer@gmail.com>
2020-02-17 17:33:54 -0800

    [B] add 150ms timeout to fetch all grades

--- backend/src/gradesapi/grades.go
+++ backend/src/gradesapi/grades.go
@@ -14,6 +14,7 @@ import (
 	"net/http"
 	"strconv"
 	"sync"
+	"time"
 )
 
 var (
@@ -310,6 +311,7 @@ func GradesForAllHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Pa
 
 			return
 		}(tok.CanvasUserID)
+		time.Sleep(150 * time.Millisecond)
 	}
 
 	wg.Wait()
