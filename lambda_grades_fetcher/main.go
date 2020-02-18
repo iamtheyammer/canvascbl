de7fa651178abfcaa339d4d0e5adcaf64ad62084
iamtheyammer <iamtheyammer@gmail.com>
2020-02-18 09:54:23 -0800

    [LGF] Remove unnecessary import

--- lambda_grades_fetcher/main.go
+++ lambda_grades_fetcher/main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/url"
 	"strings"
