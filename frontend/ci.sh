11b48de099d8d7bb1cd03379777eeff847814e79
iamtheyammer <iamtheyammer@gmail.com>
2020-02-17 20:16:39 -0800

    [I] Activate nvm

--- frontend/ci.sh
+++ frontend/ci.sh
@@ -5,6 +5,8 @@ cd frontend || exit
 case "$1" in
 "install")
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   nvm install "$(cat .nvmrc)"
   nvm use
   yarn install
