1928868561063ed4fc5a98bfd675383bc896d09b
iamtheyammer <iamtheyammer@gmail.com>
2020-05-03 15:37:36 -0700

    [I] Add S3 deploy step
    GitHub Pages deploy still present-- it'll be removed later

--- account_frontend/ci.sh
+++ account_frontend/ci.sh
@@ -16,12 +16,19 @@ case "$1" in
 	echo "Built frontend. Output is in ./build"
   ;;
 "before_deploy")
-  racv=$(NODE_DISABLE_COLORS=1 node -e 'console.log(Date.now())')
-  export REACT_APP_CURRENT_VERSION=$racv
-  echo "$ACCOUNT_CNAME" > build/CNAME
+#  racv=$(NODE_DISABLE_COLORS=1 node -e 'console.log(Date.now())')
+#  export REACT_APP_CURRENT_VERSION=$racv
+#  echo "$ACCOUNT_CNAME" > build/CNAME
+  ;;
+"deploy")
+  export AWS_ACCESS_KEY_ID=$2
+  export AWS_SECRET_ACCESS_KEY=$3
+  BUCKET_NAME=$4
+
+  aws s3 sync ./build s3://$("$BUCKET_NAME") --delete
   ;;
 *)
   echo "Usage: ./ci.sh <command>"
-  echo "Where <command> is \"install\", \"build\", or \"before_deploy\"."
+  echo "Where <command> is \"install\", \"build\", \"before_deploy\", or \"deploy\"."
   ;;
 esac
